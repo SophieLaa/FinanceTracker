2ec68dd9727cc0dbaba91bf6221d3da45985ed97
SophieeLaa <sophielaluashvili@gmail.com>
2024-07-19 01:40:36 +0400

    Add dto, using mapper

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafc35f7a8b33cd476b67dda092afe69348a94dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b7a31759cd1aa95c1c85665407dcc6d8dc6e5e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
